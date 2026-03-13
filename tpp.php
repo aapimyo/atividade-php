07a5760a047d75072964a7406b3f42ca7731ca71
Thamily <thamilyp0@gmail.com>
2026-03-13 15:05:57 -0300

    Add files via upload

---
+++ tpp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="get">
+Número 1: <input type="number" name="n1"><br><br>
+Número 2: <input type="number" name="n2"><br><br>
+<input type="submit" value="Calcular">
+</form>
+<?php 
+if(isset($_GET["n1"]) && isset($_GET)) ?>
+</body>
+</html>
